643cddbcfae40b5cd0f79cf8181b1bad644a255c
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-24 11:38:14 +0200

    creation fonction envoyer

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -5,47 +5,51 @@ class SignUp extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      email: 'Your email will appear here',
-      password: 'Your email will appear here',
-      passwordbis: 'Your password will appear here',
-      name: 'Your name will appear here',
-      lastname: 'Your last name will appear here'
+      email: "mon@email.com",
+      password: "monPassw0rd",
+      passwordbis: "monPassw0rd",
+      name: "James",
+      lastname: "Bond",
     }
+    this.updateField = this.updateField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
+  
   updateField = event => {
     this.setState ({
       [event.target.name] : event.target.value
     })
   }
 
+  handleSubmit = event => {
+    console.log('A new user was submitted: ' + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
   render () {
+    const myJSON = JSON.stringify(this.state);
+    
     return (
-      <div>
-        <input
-        onChange={this.updateField}
+      <form onSubmit={this.handleSubmit}>
+        <h1>{myJSON}</h1>
+
+        <input onChange={this.updateField}
         type="email" name="email" placeholder='please enter your email'/>
-        <h1>{this.state.email}</h1>
 
         <input onChange={this.updateField}
-        type="text" name="password" placeholder='please enter your password'/>
-        <h1>{this.state.password}</h1>
+        type="password" name="password" placeholder='please enter your password'/>
 
         <input onChange={this.updateField}
-        type="text" name="passwordbis" placeholder='please enter your password again'/>
+        type="password" name="passwordbis" placeholder='please enter your password again'/>
 
-        <h1>{this.state.passwordbis}</h1>
         <input onChange={this.updateField}
         type="text" name="name" placeholder='please enter your first name'/>
 
-        <h1>{this.state.name}</h1>
         <input onChange={this.updateField}
         type="text" name="lastname" placeholder='please enter your last name'/>
-        <h1>{this.state.lastname}</h1>
 
         <input type="submit" value="Soumettre"/>
-
-     </div>   
+     </form>   
     );
   }
 }
