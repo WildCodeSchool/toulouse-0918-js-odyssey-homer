d7052d3b7f6c8b9acbf2262cb36036ddc0cd7b60
Maitetxu Darguy <maitetxu.darguy@gmail.com>
2018-10-24 17:21:58 +0200

    Création d'un formulaire d'enregistrement

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,26 +4,70 @@ class SignUp extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            email: 'email@email.com'
+            email: "mon@email.com",
+            password: "monPassw0rd",
+            passwordVerified: "monPassw0rd",
+            name: "James",
+            lastname: "Bond"
         }
+        this.updateField = this.updateField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    updateEmailField = (event) => {
+    updateField = (event) => {
         this.setState({
-            email: event.target.value
+            [event.target.name]: event.target.value
         })
     }
 
+    handleSubmit = (event) => {
+        console.log(JSON.stringify(this.state))
+        event.preventDefault()
+    }
+
     render() {
+        const datas = JSON.stringify(this.state)
         return (
             <div>
-                <h1>{this.state.email}</h1>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={this.updateEmailField.bind(this)}
-                    autoComplete="off"
-                />
+                <form onSubmit={this.handleSubmit}>
+                    <h1>{datas}</h1>
+                    <input
+                        name="email"
+                        type="email"
+                        onChange={this.updateField.bind(this)}
+                        placeholder="E-mail"
+                        autoComplete="off"
+                    />
+                    <input
+                        name="password"
+                        type="password"
+                        onChange={this.updateField.bind(this)}
+                        placeholder="Mot de passe"
+                        autoComplete="off"
+                    />
+                    <input
+                        name="passwordVerified"
+                        type="password"
+                        onChange={this.updateField.bind(this)}
+                        placeholder="Vérification mot de passe"
+                        autoComplete="off"
+                    />
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={this.updateField.bind(this)}
+                        placeholder="Prénom"
+                        autoComplete="off"
+                    />
+                    <input
+                        name="lastname"
+                        type="text"
+                        onChange={this.updateField.bind(this)}
+                        placeholder="Nom"
+                        autoComplete="off"
+                    />
+                    <input type="submit" value="Soumettre" />
+                </form>
             </div>
         );
     }
