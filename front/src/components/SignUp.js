ba771bdaf78f58fc18f3e56db0b475fc337142c6
Claire Lebastard <claire_lebatard@hotmail.com>
2018-10-24 10:50:50 +0200

    creation des 5 champs de formulaire + bouton

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -5,11 +5,15 @@ class SignUp extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      email: 'Your email will appear here'
+      email: 'Your email will appear here',
+      password: 'Your email will appear here',
+      passwordbis: 'Your password will appear here',
+      name: 'Your name will appear here',
+      lastname: 'Your last name will appear here'
     }
   }
 
-  updateEmailField = event => {
+  updateField = event => {
     this.setState ({
       [event.target.name] : event.target.value
     })
@@ -18,10 +22,30 @@ class SignUp extends Component {
   render () {
     return (
       <div>
-        <input onChange={this.updateEmailField}
+        <input
+        onChange={this.updateField}
         type="email" name="email" placeholder='please enter your email'/>
         <h1>{this.state.email}</h1>
-      </div>   
+
+        <input onChange={this.updateField}
+        type="text" name="password" placeholder='please enter your password'/>
+        <h1>{this.state.password}</h1>
+
+        <input onChange={this.updateField}
+        type="text" name="passwordbis" placeholder='please enter your password again'/>
+
+        <h1>{this.state.passwordbis}</h1>
+        <input onChange={this.updateField}
+        type="text" name="name" placeholder='please enter your first name'/>
+
+        <h1>{this.state.name}</h1>
+        <input onChange={this.updateField}
+        type="text" name="lastname" placeholder='please enter your last name'/>
+        <h1>{this.state.lastname}</h1>
+
+        <input type="submit" value="Soumettre"/>
+
+     </div>   
     );
   }
 }
