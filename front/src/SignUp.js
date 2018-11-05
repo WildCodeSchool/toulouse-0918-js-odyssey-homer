63075bf80b08052294d7f709c4ddb473b2764bd4
Alexis Fromager <alexisfromager@live.fr>
2018-11-05 10:34:42 +0100

    020 - Création d'un formulaire d'enregistrement

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,25 +4,87 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mail: "test@test.com"
+            email: "",
+            password: "",
+            confirmPassword: "",
+            name: "",
+            lastname: ""
         };
+        this.handleSubmit = this.handleSubmit.bind(this);
     };
 
     updateEmailField = event => {
         this.setState({
-            mail: event.target.value
+            email: event.target.value
         });
+    };
+
+    updatePasswordField = event => {
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    updateConfirmPasswordField = event => {
+        this.setState({
+            confirmPassword: event.target.value
+        });
+    };
+
+    updateNameField = event => {
+        this.setState({
+            name: event.target.value
+        });
+    };
+
+    updateLastnameField = event => {
+        this.setState({
+            lastname: event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.value);
     };
 
     render() {
         return (
             <div>
-                <h1>{this.state.mail}</h1>
-                <input
-                    type="email"
-                    name="email"
-                    onChange={event => this.updateEmailField(event)}                    
-                />
+                <form onSubmit={this.handleSubmit}>
+                    <h1>{JSON.stringify(this.state,1,1)}</h1>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="mon@email.com"
+                        onChange={event => this.updateEmailField(event)}                    
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="monPassw0rd"
+                        onChange={event => this.updatePasswordField(event)}                    
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="monPassw0rd"
+                        onChange={event => this.updateConfirmPasswordField(event)}                    
+                    />
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="James"
+                        onChange={event => this.updateNameField(event)}                    
+                    />
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Bond"
+                        onChange={event => this.updateLastnameField(event)}                    
+                    />
+                    <input type="submit" value="Soumettre"/>
+                </form>
             </div>
         )
     };
