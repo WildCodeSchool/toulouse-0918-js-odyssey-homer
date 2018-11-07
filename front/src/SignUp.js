1343894396dc3dc2d97130b00a1c30b6e0942e14
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-11-07 20:07:04 +0100

    Quête finie

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -6,28 +6,72 @@ class SignUp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			email: ''
+			email: '',
+			password: '',
+			verifyPassword: '',
+			name: '',
+			surname: ''
 		};
-		this.updateEmailField = this.updateEmailField.bind(this);
+		this.updateState = this.updateState.bind(this);
+		this.submitForm = this.submitForm.bind(this);
 	}
 
-	updateEmailField(event) {
+	updateState(event) {
 		this.setState({
-			email: event.target.value
+			[event.target.name]: event.target.value
 		});
 	}
 
+	submitForm(event) {
+		event.preventDefault();
+		console.log(JSON.stringify(this.state))
+	}
+
 	render() {
-		const { email } = this.state;
 		return (
-			<email>
-				<h1>{email}</h1>
+			<form onSubmit={this.submitForm}>
+				<label htmlFor="email">Email:</label>
 				<input
-					onChange={this.updateEmailField}
-					type="email"
+					onChange={this.updateState}
+					id="email"
+					type="text"
 					name="email"
+					placeholder="Enter your email"
 				/>
-			</email>
+				<label htmlFor="password">Password:</label>
+				<input
+					onChange={this.updateState}
+					id="password"
+					name="password"
+					type="password"
+					placeholder="Enter your password"
+				/>
+				<label htmlFor="verifyPassword">Verify your password:</label>
+				<input
+					onChange={this.updateState}
+					id="verifyPassword"
+					type="password"
+					name="verifyPassword"
+					placeholder="Confirm your password"
+				/>
+				<label htmlFor="name">Name:</label>
+				<input
+					onChange={this.updateState}
+					id="name"
+					type="name"
+					name="name"
+					placeholder="Enter your name"
+				/>
+				<label htmlFor="surname">Surname:</label>
+				<input
+					onChange={this.updateState}
+					id="surname"
+					type="surname"
+					name="surname"
+					placeholder="Enter your surname"
+				/>
+				<input className="submitBtn" type="submit" value="Submit" />
+			</form>
 		);
 	}
 }
