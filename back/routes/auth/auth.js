0c087971de84c6f6502a3033c08fdc0f88689f26
Geoffroy Periot <geoffroy.periot@gmail.com>
2018-11-26 16:32:26 +0100

    quete 040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,12 +1,29 @@
-const  express  =  require('express');
+const express  =  require('express');
 const router = express.Router();
+const connection = require('../../helpers/db');
+
 
 router.get("/", (req,res)=>{
     res.send("youhou");
 })
 
 router.post('/signup', function(req, res, next) {
-  res.send('I am in POST signup');
+  
+  // req.body = {
+  //   email: "best@adresse.kum",
+  //   password: "thisPWsuxx",
+  //   name: "Regis",
+  //   lastname: "Robert"
+  // }
+  connection.query(
+    `INSERT INTO users (email, password, name, lastname) VALUES ('${req.body.email}', '${req.body.password}', '${req.body.name}', '${req.body.lastname}')`,    
+    (err, results) => {
+      if (err) {
+        res.status(500).end();
+      } else {
+        res.end();
+      }
+    })
 });
 
 module.exports = router;
