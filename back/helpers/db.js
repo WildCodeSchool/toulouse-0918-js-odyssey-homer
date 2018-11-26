aa587080079ffb7d6eed771db0980df64a499a27
Julien Chapron <julien.chapron@protonmail.ch>
2018-11-26 17:15:59 +0100

    supp identifiant et pssword

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : 'Lemans72',
+  user     : '',
+  password : '',
   database : 'odyssee'
 });
 module.exports  =  connection;
