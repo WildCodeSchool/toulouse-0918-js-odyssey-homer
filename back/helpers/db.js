608ab37e2e1ad7becda939ef2c7c74c4f2dcb97a
Julien Chapron <julien.chapron@protonmail.ch>
2018-11-27 10:54:00 +0100

    changement des identifiants

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Lemans72',
+  password : 'test',
   database : 'odyssee'
 });
 module.exports  =  connection;
