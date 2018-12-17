9ef685754c06c3e63f575f5ce1787cce4ddaa9bb
Claire Lebastard <claire_lebatard@hotmail.com>
2018-12-17 15:24:27 +0100

    quete 060 all good

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,7 @@ import { Container } from 'reactstrap';
 class App extends Component {
   render() {
     return (
-      <Container fluid className="App bg-color" style={{minHeight: "100vh"}}>
+      <Container fluid className="bg-color d-flex align-items-center " style={{minHeight: "100vh"}}>
         <SignUp/>
       </Container>
     );
--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -1,93 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { Row, Col, Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class SignUp extends Component {
   
   constructor (props) {
     super(props);
     this.state = {
-      email: "mon@email.com",
-      password: "monPassw0rd",
-      passwordbis: "monPassw0rd",
-      name: "James",
-      lastname: "Bond",
-      flash: ""
+      email: " ",
+      password: "",
+      passwordbis: "",
+      name: "",
+      lastname: "",
+      flash: {}
     }
     this.updateField = this.updateField.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   updateField = event => {
-    this.setState ({
-      [event.target.name] : event.target.value
+    this.setState({
+      [event.target.name]: event.target.value     
     })
   }
 
   handleSubmit = event => {
-    // console.log('A new user was submitted: ' + JSON.stringify(this.state));
     axios.post('/auth/signup', this.state) //Requete
-      .then(response => response.data) // executé si ça se passe bien
-      .then(data => 
-        this.setState({flash: data.flash})
+      .then(
+        response => toast.success('User has been signed up!')
+      ) // executé si ça se passe bien
+      .catch(
+        err => toast.error(err.response.data.flash)
       )
-      .catch(err =>                     // executé si erruer
-        this.setState ({flash: err.response.data.flash}))
     event.preventDefault();
   } 
 
   render () {
     const myJSON = JSON.stringify(this.state);    
     return (
-      <Row className="bg-none align-items-center mx-5" >
-        <Col xs={12} sm={6} className="text-center">
+      <Row className="bg-none mx-5 flex-grow-1" >
+        <Col xs={12} md={6} className="text-center">
           <img src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png" alt="Homer Simpson"
           style={{minHeight: "100%"}} />
         </Col> 
-        <Col xs={12}  sm={6} className="text-align-center my-5">
-          
+        <Col xs={12} md={6} className="text-align-center my-5">       
           <h5 className="mb-2"><strong>Sign up !</strong></h5>          
-          <Form onSubmit={this.handleSubmit} className="submit-form">
+          <Form
+            onSubmit={this.handleSubmit} 
+            className="submit-form">
             <FormGroup>
               <Label for="email">Email</Label>
-              <Input 
+              <Input required
                 className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0"
                 onChange={this.updateField}
                 type="email" name="email" id="email" placeholder="Enter your email" />
             </FormGroup>
             <FormGroup>
               <Label for="password">Password</Label>
-              <Input
+              <Input required
                 className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0"
                 onChange={this.updateField}
                 type="password" name="password" id="password" placeholder="Enter your password" />
             </FormGroup>
             <FormGroup>
-              <Label for="password">Password Copy</Label>
-              <Input
-                className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0"
+              <Label for="password">Password copy</Label>
+              <Input required
+                className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0 rounded-0"
                 onChange={this.updateField}
-                type="passwordbis" name="passwordbis" id="passwordbis" placeholder="Re-enter your password" />
+                type="password" name="passwordbis" id="passwordbis" placeholder="Re-enter your password" />
             </FormGroup>
             <FormGroup>
               <Label for="name">Name</Label>
-              <Input
+              <Input required
                 className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0"
                 onChange={this.updateField}
                 type="text" name="name" id="name" placeholder="Enter your name"/>
             </FormGroup>
             <FormGroup>
               <Label for="lastname">Lastname</Label>
-              <Input
+              <Input required
                 className="ml-0 pl-0 border-bottom-1 border-left-0 border-right-0 border-top-0"
-                onChange={this.updateField}type="text" name="lastname" id="lastname" placeholder="Enter your lastname"/>
+                onChange={this.updateField}
+                type="text" name="lastname" id="lastname" placeholder="Enter your lastname"/>
             </FormGroup>
-            <div className="d-flex">
-              <Button type="submit" value="SUBMIT" className="d-flex align-items-right"><strong>SUBMIT</strong></Button>
-            </div>
-            <h1>{myJSON}</h1>
+            <Button type="submit" value="SUBMIT" className="float-right"><strong>SUBMIT</strong></Button>
+            {/* <p>{myJSON}</p> */}
+
+            <ToastContainer
+              // store={ToastStore}
+              // lightBackground
+              // position={ToastContainer.POSITION.BOTTOM_LEFT}
+            />
           </Form>
-          </Col>
+        </Col>
       </Row>
     );
   }
